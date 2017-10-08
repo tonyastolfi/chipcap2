8b84067ad33315673a0b38f5705e7bf1011812ca
Tony Astolfi <tastolfi@gmail.com>
2017-10-08 19:37:35 +0000

    fix rc3.d symlink name

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ sed -i -e "s,CHIPCAP2_SOURCE_DIR,${srcdir},g" /etc/init.d/chipcap2
 sed -i -e "s,CHIPCAP2_REPORTING_LOCATION,${1},g" /etc/init.d/chipcap2
 chmod +x /etc/init.d/chipcap2
 rm -f /etc/rc3.d/chipcap2
-ln -s /etc/init.d/chipcap2 /etc/rc3.d/chipcap2
+ln -s /etc/init.d/chipcap2 /etc/rc3.d/S06chipcap2
